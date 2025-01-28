223916999d4cf785f84f2814fc856929fe70209f
HazzReghem <thibault.roussel@gmx.com>
2025-01-28 11:06:02 +0100

    first commit

---
+++ Ecogarden/src/Entity/Meteo.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MeteoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MeteoRepository::class)]
+class Meteo
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column]
+    private array $data = [];
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $last_fetched_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setData(array $data): static
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function getLastFetchedAt(): ?\DateTimeImmutable
+    {
+        return $this->last_fetched_at;
+    }
+
+    public function setLastFetchedAt(\DateTimeImmutable $last_fetched_at): static
+    {
+        $this->last_fetched_at = $last_fetched_at;
+
+        return $this;
+    }
+}
